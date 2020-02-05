3763afeaa18463947fd0d946e4b5d9463882627f
Yijiang Huang <yijiangh@mit.edu>
2020-02-05 10:45:15 -0500

    move scipy, pycddlib out of setup.py

--- setup.py
+++ setup.py
@@ -22,11 +22,12 @@ requirements = [
     # Until COMPAS reaches 1.0, we pin major.minor and allow patch version updates
     # 'compas>=0.11,<0.12',
     'compas',
-    'scipy',
-    'pycddlib',
     # ! before PR is merged...
     # 'compas_fab',
     # 'pychoreo',
+    # ! runtime dependencies
+    # 'scipy>=1.4.1',
+    # 'pycddlib',
 ]
 keywords_list = ['robotic assembly', 'double-tangent frame structure', 'digital fabrication', 'architecture']
 
